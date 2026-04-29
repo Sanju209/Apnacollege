0ee4d978f2eb6afbebfa4bc2db5df3e9a3ca0ab3
Sanju209 <sanjay.c2096@gmail.com>
2026-04-29 13:07:58 +0000

    idk

--- ttt/script.js
+++ ttt/script.js
@@ -1,17 +1,29 @@
-let boxes = document.getElementsByClassName(".box");
+let boxes = document.querySelectorAll(".box");
+let rst = document.querySelectorAll("#rst");
 
 let turnO = true;
 
-const winPat = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8]
-                [3, 6, 9], [1, 5, 9], [3, 5, 7]];
+const winPat = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7],
+                [2, 5, 8], [0, 4, 8], [2, 4, 6]];
 
 boxes.forEach((btn) => {
     btn.addEventListener("click", function () {
     if(turnO && btn.innerText === "") {
-       btn.innerText = "0";
+        btn.innerText = "0";
+        turnO = false;
     }
     else if(btn.innerText === "") {
         btn.innerText = "X";
+        turnO = true;
     }
-    })
-})
+    checkWin();
+    });
+});
+
+function checkWin() {
+    for(const pattern of winPat) {
+        if(boxes[pattern[0]].innerText === boxes[pattern[1]].innerText && boxes[pattern[0]].innerText === boxes[pattern[2]]) {
+            rst.innerText = "win";
+        }
+    }
+}
